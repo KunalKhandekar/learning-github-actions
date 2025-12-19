18e38fe2c05f94210a0358c17a7a5aedaa2443e7
Sahil Khandekar <134169718+KunalKhandekar@users.noreply.github.com>
2025-12-19 14:31:22 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+import http from "http";
+
+const PORT = process.env.Porject_PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/health") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+      })
+    );
+    return;
+  }
+
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Server is running");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
